35706b91f2019a93f10bae5e369fd411f1116893
Haorui HE <68176557+HarryHe11@users.noreply.github.com>
2022-09-19 14:12:37 +0800

    Add files via upload

--- models/Bert.py
+++ models/Bert.py
@@ -8,24 +8,26 @@ class Config(object):
     """Config for parameters"""
     def __init__(self, dataset_path):
         self.model_name = 'Bert_Classification_Model'
-        self.train_path = dataset_path + '/data/train.csv'                                # train set
-        self.dev_path = dataset_path + '/data/dev.csv'                                    # val set
-        self.test_path = dataset_path + '/data/test_set.xlsx'                                  # test set
+        self.train_path = dataset_path + '/data/new_train.csv'                                # train set
+        self.dev_path = dataset_path + '/data/new_train.csv'                                    # val set
+        self.test_path = dataset_path + '/data/test.csv'                                  # test set
         self.content_key = 'text'
         self.label_key = 'label'
-        self.class_list = [0, 1, 2, 3]                                                       # label list
+        self.label_dict = {"FAVOR": 0, "AGAINST": 1, "NEITHER": 2}
+        self.class_list = [0, 1, 2]                                                       # label list
         self.num_classes = len(self.class_list)  # number of labels
         self.save_path = dataset_path + '/saved_dict/' + self.model_name + '.ckpt'        # path for model storing
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')        # devices
 
-        self.num_epochs = 10                                            # number of training epochs
-        self.patience = 5                                               # patience for early stopping
-        self.batch_size = 16                                            # mini-batch
+        self.num_epochs = 10                                             # number of training epochs
+        self.patience = 3                                               # patience for early stopping
+        self.batch_size = 32                                            # mini-batch
         self.pad_size = 64                                              # max sequence size
-        self.learning_rate = 5e-5                                       # learning rate
-        self.bert_path = "bert-base-uncased"                            # Bert model
+        self.learning_rate = 1e-5                                      # learning rate
+        self.bert_path = "bert-base-uncased"            #               # Bert model
         self.tokenizer = AutoTokenizer.from_pretrained(self.bert_path)  # tokenizer
-        self.hidden_size = 768                                          # hidden size of Bert
+        self.hidden_size = 768 
+        self.dropout_rate = 0.5                                         # hidden size of Bert
 
 
 class Model(nn.Module):
@@ -35,10 +37,12 @@ class Model(nn.Module):
         for param in self.bert.parameters():
             param.requires_grad = True
         self.fc = nn.Linear(config.hidden_size, config.num_classes)
+        self.dropout = nn.Dropout(config.dropout_rate)
 
     def forward(self, x):
         context = x[0]
         mask = x[2]
-        _, pooled = self.bert(context, attention_mask=mask)
-        out = self.fc(pooled)
-        return out
+        _, pooler_output = self.bert(context, attention_mask=mask)
+        pooled = self.dropout(pooler_output)
+        logits = self.fc(pooled)
+        return logits
